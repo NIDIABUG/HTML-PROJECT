9b1ae6058451c0e015f724f6ee194e55f401b403
NIDIABUG <nidiastudioph@gmail.com>
2023-07-03 17:06:15 +0800

    some more things

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -1,26 +1,26 @@
-var value = "";
+// var value = "";
 
-function addNumber(number){
-    value += number;
-    updateDisplay();
-}
+// function addNumber(number){
+//     value += number;
+//     updateDisplay();
+// }
 
-function setOperand(operand){
-    if(value.endsWith('+') || value.endsWith('-') || value.endsWith('*') || value.endsWith('/')){
-        return;
-    }
-    value += operand;
-    updateDisplay();
-    console.log(value);
-}
+// function setOperand(operand){
+//     if(value.endsWith('+') || value.endsWith('-') || value.endsWith('*') || value.endsWith('/')){
+//         return;
+//     }
+//     value += operand;
+//     updateDisplay();
+//     console.log(value);
+// }
 
-function calculate(equal){
+// function calculate(equal){
 
-}
+// }
 
-function updateDisplay() {
-    var displayElement = document.getElementById("display");
-    displayElement.textContent = value; // Update the text content of the display element
-    console.log(value);
-  }
+// function updateDisplay() {
+//     var displayElement = document.getElementById("display");
+//     displayElement.textContent = value; // Update the text content of the display element
+//     console.log(value);
+//   }
   
